627bf332f11b1ff0d7adad0f315c0e66070a8c27
Nhat Huy Le <65520688+huylee99@users.noreply.github.com>
2024-10-12 00:06:17 -0700

    feat: add some important features (#1)
    * feat: add minus function
    
    * feat: add divide function
    
    * chore: release v1.0.1

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,14 @@
-import { add } from "./utils";
+import { add, divide, minus } from "./utils";
 import { test, expect } from "vitest";
 
 test("add", () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test("minus", () => {
+    expect(minus(2, 1)).toBe(1);
+})
+
+test("divide", () => {
+    expect(divide(4, 2)).toBe(2);
+})
