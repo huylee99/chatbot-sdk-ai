54626d2d5949f10f0a9f9672029154679a5d0eb4
lee <nhathuyle083@outlook.com>
2024-10-12 00:20:14 -0700

    fix: haha func

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,5 @@ export const divide = (a: number, b: number) => a / b;
 export const multiply = (a: number, b: number) => a * b;
 
 export const hehe = () => "hehe"
+
+export const haha = () => "haha"
