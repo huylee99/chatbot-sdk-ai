e624d03120876e624a48ba01b793b89d8352e098
lee <nhathuyle083@outlook.com>
2024-10-11 23:51:01 -0700

    feat: add minus function

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,10 @@
-import { add } from "./utils";
+import { add, minus } from "./utils";
 import { test, expect } from "vitest";
 
 test("add", () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test("minus", () => {
+    expect(minus(2, 1)).toBe(1);
+})
