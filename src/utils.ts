3b8ce1749ec94ba679a4438999477f3682de7025
Nhat Huy Le <65520688+huylee99@users.noreply.github.com>
2024-10-12 00:23:48 -0700

    feat: add func hehe (#3)
    * feat: add func hehe
    
    * fix: haha func

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,7 @@ export const minus = (a: number, b: number) => a - b;
 export const divide = (a: number, b: number) => a / b;
 
 export const multiply = (a: number, b: number) => a * b;
+
+export const hehe = () => "hehe"
+
+export const haha = () => "haha"
