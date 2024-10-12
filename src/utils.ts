8cc427102e68057215b2ddc8d5a3a906fcc41056
Nhat Huy Le <65520688+huylee99@users.noreply.github.com>
2024-10-12 00:18:14 -0700

    feat: add multiply function (#2)

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,5 @@ export const add = (a: number, b: number) => a + b;
 export const minus = (a: number, b: number) => a - b;
 
 export const divide = (a: number, b: number) => a / b;
+
+export const multiply = (a: number, b: number) => a * b;
