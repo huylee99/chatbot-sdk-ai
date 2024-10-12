4decaa84511cd7aba633a8832ce75f1fee2b8db5
lee <nhathuyle083@outlook.com>
2024-10-12 00:18:47 -0700

    feat: add func hehe

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,5 @@ export const minus = (a: number, b: number) => a - b;
 export const divide = (a: number, b: number) => a / b;
 
 export const multiply = (a: number, b: number) => a * b;
+
+export const hehe = () => "hehe"
